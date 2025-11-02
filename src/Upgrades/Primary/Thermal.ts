2a140a5d1826b98870ee925308453e393ee2db50
particle <notnane3@gmail.com>
2025-11-01 21:18:40 -0400

    Thermal upgrade

--- src/Upgrades/Primary/Thermal.ts
+++ src/Upgrades/Primary/Thermal.ts
@@ -1,5 +1,6 @@
 import PrimaryUpgrade from '../PrimaryUpgrade';
 import PlayerEntity from '../../Entities/PlayerEntity';
+import { EntityStateFlags } from '../../Enums/Flags';
 
 export default class Thermal extends PrimaryUpgrade {
     constructor(player: PlayerEntity) {
@@ -7,6 +8,13 @@ export default class Thermal extends PrimaryUpgrade {
     }
 
     public applyUpgrade(): void {
-        //lol who even uses this
+        //set thermal flag to 1
+        this.owner.thermal = 1;
+
+        //update client
+        this.owner.fieldManager.safeUpdate({
+            states: [EntityStateFlags.FIRST_PERSON_UPDATE],
+            firstPersonFields: ['thermal']
+        });
     }
 }
